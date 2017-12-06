4b13a2ad5778c33ae4b07b394e09e6147f06dd31
xxy130330 <yangxue0708@gmail.com>
2017-12-06 12:55:55 -0800

    import-export-demo

---
+++ app/cal.js
@@ -0,0 +1,6 @@
+export default function (x, y) {
+    return x + y;
+}
+
+export const subtract = (x, y) => x - y;
+export const multiply = (x, y) => x * y;
--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import * as math from './cal';
+console.log('math:', math);
+
+
+
+
+//import addStuff, {subtract, multiply} from './cal';
+
+console.log('5 + 7 =', math.default(5,7));
+console.log('8 - 9 =', math.subtract(8, 9));
+console.log('4 * 6 =', math.multiply(4, 6));
+
+
+
+//console.log('App working: open app/index.js to start exploring ES6');
